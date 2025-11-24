223b15d76ebc32666e1e9753d270c5bec0ced238
Javichotkd <javiermartingutierrez.sant@gmail.com>
2025-11-24 13:29:54 +0100

    multiplicacion y division

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,8 @@ public class Main {
         System.out.println("===== CALCULADORA SIMPLE =====");
         System.out.println("1. Sumar");
         System.out.println("2. Restar");
+        System.out.println("3. Multiplicar");
+        System.out.println("4. Dividir");
         System.out.print("Elige una opción: ");
         opcion = sc.nextInt();
 
@@ -30,6 +32,30 @@ public class Main {
                 resultado = num1 - num2;
                 System.out.println("Resultado: " + resultado);
                 break;
+
+            case 3:
+                System.out.print("Introduce el primer número: ");
+                num1 = sc.nextDouble();
+                System.out.print("Introduce el segundo número: ");
+                num2 = sc.nextDouble();
+                resultado = num1 * num2;
+                System.out.println("Resultado: " + resultado);
+                break;
+
+            case 4:
+                System.out.print("Introduce el primer número: ");
+                num1 = sc.nextDouble();
+                System.out.print("Introduce el segundo número: ");
+                num2 = sc.nextDouble();
+
+                if (num2 == 0) {
+                    System.out.println("Error: No se puede dividir entre cero.");
+                } else {
+                    resultado = num1 / num2;
+                    System.out.println("Resultado: " + resultado);
+                }
+                break;
+
             default:
                 System.out.println("Opción no válida.");
         }
