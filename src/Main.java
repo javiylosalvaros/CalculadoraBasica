6641b274e0901f10fa335a517ca757e1c5856787
Javichotkd <javiermartingutierrez.sant@gmail.com>
2025-11-24 13:44:21 +0100

    mcm y mcd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 import java.util.Scanner;
 public class Main {
+
+    public static int mcd(int a, int b) {
+        while (b != 0) {
+            int t = b;
+            b = a % b;
+            a = t;
+        }
+        return Math.abs(a);
+    }
+
+    public static int mcm(int a, int b) {
+        return Math.abs(a * b) / mcd(a, b);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int opcion;
@@ -11,6 +25,8 @@ public class Main {
         System.out.println("2. Restar");
         System.out.println("3. Multiplicar");
         System.out.println("4. Dividir");
+        System.out.println("5. Máximo Común Divisor (MCD)");
+        System.out.println("6. Mínimo Común Múltiplo (MCM)");
         System.out.print("Elige una opción: ");
         opcion = sc.nextInt();
 
@@ -55,6 +71,23 @@ public class Main {
                     System.out.println("Resultado: " + resultado);
                 }
                 break;
+
+            case 5:
+                System.out.print("Introduce el primer número entero: ");
+                int a1 = sc.nextInt();
+                System.out.print("Introduce el segundo número entero: ");
+                int b1 = sc.nextInt();
+                System.out.println("MCD: " + mcd(a1, b1));
+                break;
+
+            case 6:
+                System.out.print("Introduce el primer número entero: ");
+                int a2 = sc.nextInt();
+                System.out.print("Introduce el segundo número entero: ");
+                int b2 = sc.nextInt();
+                System.out.println("MCM: " + mcm(a2, b2));
+                break;
+
 
             default:
                 System.out.println("Opción no válida.");
