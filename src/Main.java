c6158f7a939598d685b7beb9ced8618447cda10d
Javichotkd <javiermartingutierrez.sant@gmail.com>
2025-11-24 13:03:02 +0100

    sumas y restas

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        double num1, num2, resultado;
+
+
+        System.out.println("===== CALCULADORA SIMPLE =====");
+        System.out.println("1. Sumar");
+        System.out.println("2. Restar");
+        System.out.print("Elige una opción: ");
+        opcion = sc.nextInt();
+
+        switch (opcion) {
+            case 1:
+                System.out.print("Introduce el primer número: ");
+                num1 = sc.nextDouble();
+                System.out.print("Introduce el segundo número: ");
+                num2 = sc.nextDouble();
+                resultado = num1 + num2;
+                System.out.println("Resultado: " + resultado);
+                break;
+
+            case 2:
+                System.out.print("Introduce el primer número: ");
+                num1 = sc.nextDouble();
+                System.out.print("Introduce el segundo número: ");
+                num2 = sc.nextDouble();
+                resultado = num1 - num2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            default:
+                System.out.println("Opción no válida.");
+        }
+    }
+}
